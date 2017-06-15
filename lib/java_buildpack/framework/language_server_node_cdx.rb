91fc7411241572c29dc048604f3d8287cd9b6d63
Sosin <pavel.sosin@sap.com>
2017-06-15 14:31:03 +0300

    Install nodejs

--- lib/java_buildpack/framework/language_server_node_cdx.rb
+++ lib/java_buildpack/framework/language_server_node_cdx.rb
@@ -53,7 +53,7 @@ module JavaBuildpack
         @logger.debug { "NodeRoot: #{@application.root}   NodeBin: #{nodeBin}"}
 
         #FileUtils.chmod "a=rx", nodeBin
-        FileUtils.ln_s "/home/vcap/app/.java-buildpack/language_server_node_cdx/nodejs/bin/npm", "/usr/bin/npm", force: true
+        #FileUtils.ln_s "/home/vcap/app/.java-buildpack/language_server_node_cdx/nodejs/bin/npm", "/usr/bin/npm", force: true
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
