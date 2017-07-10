3ca253c03b33e5ffcb0a66d3f68c3c6303b08b4b
amiramw <wingarte@gmail.com>
2017-07-10 15:03:48 +0300

    Update language_server_bin_exec_jdt.rb

--- lib/java_buildpack/framework/language_server_bin_exec_jdt.rb
+++ lib/java_buildpack/framework/language_server_bin_exec_jdt.rb
@@ -1,18 +1,5 @@
 # Encoding: utf-8
-# Cloud Foundry Java Buildpack
-# Copyright 2013-2017 the original author or authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# TODO License.
 
 require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/framework'
@@ -53,8 +40,6 @@ module JavaBuildpack
       def release
 
         environment_variables = @droplet.environment_variables
-#        myBasedir = @configuration["env"]["basedir"]
-#        environment_variables.add_environment_variable(ENV_PREFIX + "basedir", myBasedir)
 
         myWorkdir = @configuration["env"]["workdir"]
         environment_variables.add_environment_variable(ENV_PREFIX + "workdir", myWorkdir)
